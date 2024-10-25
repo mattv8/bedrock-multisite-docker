4708266ea3e0c2a8d6be401b37ca768d4476f4ed
mattv8 <matt@visnovsky.us>
2024-10-25 11:57:12 -0600

    Remove unused

--- config/application.php
+++ config/application.php
@@ -164,11 +164,6 @@ Config::define('PATH_CURRENT_SITE', env('PATH_CURRENT_SITE') ?: '/');
 Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
 Config::define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
 
-define('WP_ALLOW_REPAIR', true);
-
-// Set the cookie domain to false to avoid issues in local development
-Config::define('COOKIE_DOMAIN', false);
-
 Config::apply();
 
 /**
